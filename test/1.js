c87494c88ed4a4c3aa8c8835d806e07f79730134
Maxim Ryndin <masim_r@mail.ru>
2016-04-17 08:59:32 +0300

    Correct code style.

--- test/1.js
+++ test/1.js
@@ -1,13 +1,13 @@
 'use strict;'
 
-assert = require('assertthat');
+var assert = require('assertthat');
+
+// One can use binary literals, thanks to ES6
+function calculate(a, b) {
+  return eval('0b' + a) + eval('0b' + b);
+}
 
 describe('Calculator', function () {
-
-  // One can use binary literals, thanks to ES6
-  function calculate(a, b) {
-    return eval('0b' + a) + eval('0b' + b);
-  }
 
   it('should calculate 10 + 10', function () {
     assert.that(calculate('10', '10')).is.equalTo(4);
