8fcfa931bec0883abd2beaeb14698c6c0bcfa1a5
Maxim Ryndin <masim_r@mail.ru>
2016-04-17 08:59:43 +0300

    Correct code style in the part 2.

--- test/2.js
+++ test/2.js
@@ -1,66 +1,67 @@
 'use strict;'
 
-assert = require('assertthat');
+var assert = require('assertthat');
+
+function plus(arg) {
+  var value;
+  if (typeof  arg === 'function') {
+    value = arg();
+  } else {
+    value = arg;
+  }
+  return function (n) {
+    return n + value;
+  }
+}
+
+function times(arg) {
+  var value;
+  if (typeof  arg === 'function') {
+    value = arg();
+  } else {
+    value = arg;
+  }
+  return function (n) {
+    return n * value;
+  }
+}
+
+function minus(arg) {
+  return function (n) {
+    return n - arg();
+  }
+}
+
+function dividedBy(arg) {
+  return function (n) {
+    return n / arg();
+  }
+}
+
+function _number(n) {
+  return function (arg) {
+    if (arg) {
+      return arg(n);
+    }
+
+    return function () {
+      return n;
+    }
+  }
+}
+
+var zero = _number(0);
+var one = _number(1);
+var two = _number(2);
+var three = _number(3);
+var four = _number(4);
+var five = _number(5);
+var six = _number(6);
+var seven = _number(7);
+var eight = _number(8);
+var nine = _number(9);
 
 describe('Currying', function () {
-  function plus(arg) {
-    var value;
-    if (typeof  arg === 'function') {
-      value = arg();
-    } else {
-      value = arg;
-    }
-    return function (n) {
-      return n + value;
-    }
-  }
-
-  function times(arg) {
-    var value;
-    if (typeof  arg === 'function') {
-      value = arg();
-    } else {
-      value = arg;
-    }
-    return function (n) {
-      return n * value;
-    }
-  }
-
-  function minus(arg) {
-    return function (n) {
-      return n - arg();
-    }
-  }
-
-  function dividedBy(arg) {
-    return function (n) {
-      return n / arg();
-    }
-  }
-
-  function _number(n) {
-    return function (arg) {
-      if (arg) {
-        return arg(n);
-      }
-
-      return function () {
-        return n;
-      }
-    }
-  }
-
-  var zero = _number(0);
-  var one = _number(1);
-  var two = _number(2);
-  var three = _number(3);
-  var four = _number(4);
-  var five = _number(5);
-  var six = _number(6);
-  var seven = _number(7);
-  var eight = _number(8);
-  var nine = _number(9);
 
   it('should perform plus', function () {
     assert.that(zero(plus(one())) === 1).is.true();
